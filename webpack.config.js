90ea31bc0afe0a41c2bd0d6c8cbee7ca3b050405
Jakub Biesiada <biesiadajakub@icloud.com>
2018-07-13 19:56:10 +0200

    Update

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env;
+
+let libraryName = 'react-parlx';
+
+let outputFile, mode;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = libraryName + '.min.js';
+} else {
+  mode = 'development';
+  outputFile = libraryName + '.js';
+}
+
+const config = {
+  mode: mode,
+  entry: __dirname + '/src/index.js',
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'parlx.js': {
+      root: 'Parlx',
+      commonjs2: 'parlx.js',
+      commonjs: 'parlx.js',
+      amd: 'parlx.js'
+    }
+  },
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: 'ReactParlx',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  }
+};
+
+module.exports = config;
