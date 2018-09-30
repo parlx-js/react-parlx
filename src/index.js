9f1c51091e55cd8546c8404a3796e849bf81d2ca
Jakub Biesiada <biesiadajakub@icloud.com>
2018-09-30 12:55:48 +0200

    Clean up

--- src/index.js
+++ src/index.js
@@ -9,24 +9,21 @@ export default class ReactParlx extends Component {
   }
 
   componentDidMount() {
-    const { options } = this.props;
+    const { options, parlxMove } = this.props;
     const parlx = new Parlx(this.el.current, options);
 
-    if (this.props.parlxMove) {
-      this.el.current.addEventListener('parlxMove', this.output);
-    }
+    if (parlxMove) this.el.current.addEventListener('parlxMove', this.output);
   }
 
   output = e => this.props.parlxMove(e.detail.move);
 
   render() {
+    const { className, style, overlay, children } = this.props;
+
     return (
-      <div
-        className={this.props.className || 'parallax'}
-        style={this.props.style}
-        ref={this.el}>
-        {this.props.overlay ? <div className="overlay" /> : null}
-        {this.props.children}
+      <div className={className || 'parallax'} style={style} ref={this.el}>
+        {overlay ? <div className="overlay" /> : null}
+        {children}
       </div>
     );
   }
