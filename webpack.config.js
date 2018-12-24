769dd234fcf031cbeefb5d82abdd558333eaaff5
Jakub Biesiada <biesiadajakub@icloud.com>
2018-12-24 10:03:26 +0100

    Clean up

--- webpack.config.js
+++ webpack.config.js
@@ -44,10 +44,8 @@ const config = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        loader: 'babel-loader',
+        exclude: /node_modules/
       }
     ]
   }
